a65d35fef5046c674ab21568d273f18a2d82c5e2
Arushi <arushi.0192@gmail.com>
2024-03-23 15:46:05 +0100

    additional EC2

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ provider "aws" {
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
+  count = 2
 
   tags = {
     Name = "ArushiExample"
