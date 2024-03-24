2e84ede2db8bc784c0385c926cf1a044a53d4584
Arushi <arushi.0192@gmail.com>
2024-03-24 22:31:47 +0100

    State file

--- main.tf
+++ main.tf
@@ -32,30 +32,3 @@ resource "aws_instance" "example_server" {
     Name = var.ec2_name
   }
 }
-/*
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  } 
-} */
--- variables.tf
+++ variables.tf
@@ -1,17 +1,3 @@
-/*
-variable "bucket_name" {
-  description = "The name of the S3 bucket. Must be globally unique."
-  type        = string
-  default = "remotestates3bucket23032024"
-}
-
-variable "table_name" {
-  description = "The name of the DynamoDB table. Must be unique in this AWS account."
-  type        = string
-  default = "terraform-remote-state-dynamo"
-}
-*/
-
 variable "ec2_name" {
   type = string
 }
